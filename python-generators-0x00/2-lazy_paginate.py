6c2ada0f93a7e391575d2695a86307ff9b93ad45
Ibn-elaraby37 <h.ibrahim3705@gmail.com>
2025-07-06 23:33:41 +0300

    lazy paginate

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,10 @@
+
+
+def lazy_paginate(page_size):
+    offset = 0
+    while True:
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page
+        offset += page_size
