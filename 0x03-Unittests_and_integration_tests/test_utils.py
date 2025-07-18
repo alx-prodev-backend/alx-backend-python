6c6484afd9f577d72a7dbe4b8e1a835c7cd1611c
Ibn-elaraby37 <h.ibrahim3705@gmail.com>
2025-07-19 01:39:54 +0300

    Mock assert 01

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -40,3 +40,22 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+class TestMemoize(unittest.TestCase):
+    """Test memoization behavior of the memoize decorator"""
+
+    def test_memoize(self):
+        """Test that memoized result caches and only calls original method once"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            obj = TestClass()
+            self.assertEqual(obj.a_property(), 42)
+            self.assertEqual(obj.a_property(), 42)
+            mock_method.assert_called_once()
