2cad237ff83364d4a4ffd22e5975afd7a3dba829
Ibn-elaraby37 <h.ibrahim3705@gmail.com>
2025-07-26 17:08:22 +0300

    long line

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,7 +27,8 @@ class TestGithubOrgClient(unittest.TestCase):
             "_public_repos_url",
             new_callable=PropertyMock
         ) as mock_repos_url:
-            mock_repos_url.return_value = "https://api.github.com/orgs/testorg/repos"
+            mock_repos_url.return_value = (""
+            "https://api.github.com/orgs/testorg/repos")
 
             client = GithubOrgClient("testorg")
             result = client.public_repos()
